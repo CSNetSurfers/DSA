969408cd82d16cd8b4a644fa62149df3468afe95
yashodaa <yashodara13@gmail.com>
2015-03-08 13:46:54 -0700

    Listable

--- RealEstate/src/org/realEstate/Listable.java
+++ RealEstate/src/org/realEstate/Listable.java
@@ -1,5 +1,14 @@
 package org.realEstate;
 
 public interface Listable {
+	
+	// compares the current Listable object to other object,
+	// if those two objects are equal, this method returns a zero.
+	// if current object is < other object, returns a negative value.
+	// if current object is > other object, returns a positive value.
+	public abstract int compareTo(Listable other);
+	
+	// returns a copy of the current Listable object
+	public abstract Listable copy();
 
 }
