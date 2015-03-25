e9cb428acde538c764eb02d9a6c5e37f1ed2bc7b
lushaka <lushakanisansala@ymail.com>
2015-03-25 21:55:53 +0530

    outline

--- RealEstate/src/org/realEstate/HouseFile.java
+++ RealEstate/src/org/realEstate/HouseFile.java
@@ -1,26 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package org.realEstate;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+/**
+ *
+ * @author user
+ */
 public class HouseFile {
-
-	private static BufferedReader readFile;
+    
+        private static BufferedReader readFile;
 	private static PrintWriter writeFile;
 	private static boolean readFileOpen = false;
 	private static boolean writeFileOpen = false;
 	
 	// Holds "next" line from file
-	private static String readString =""; 
+	private static String line =""; 
 	
 	// Equals null if at end of file
 	public static void reset() throws IOException
-	
 	// Reset file for reading
 	{
 	if (readFileOpen) readFile.close();
 	if (writeFileOpen) writeFile.close();
 	
-	File file=new File("houses.dat");
+	File file=new File("houses.txt");
 	
 	//Creates a new file called houses.dat, if there is no such file existing.
 	if(!(file.exists())){
@@ -28,9 +40,9 @@ public class HouseFile {
 	}
 	
 	//Reads the file
-	readFile = new BufferedReader(new FileReader("houses.dat"));
+	readFile = new BufferedReader(new FileReader("houses.txt"));
 	readFileOpen = true;
-	readString = readFile.readLine();
+	line = readFile.readLine();
 	}
 	
 	// Reset file for writing
@@ -38,7 +50,7 @@ public class HouseFile {
 	{
 	if (readFileOpen) readFile.close();
 	if (writeFileOpen) writeFile.close();
-	writeFile = new PrintWriter(new FileWriter("houses.dat"));
+	writeFile = new PrintWriter(new FileWriter("houses.txt"));
 	writeFileOpen = true;
 	}
 	
@@ -47,7 +59,7 @@ public class HouseFile {
 	// information available in it
 	public static boolean moreHouses()
 	{
-	if (!readFileOpen || (readString == null))
+	if (!readFileOpen || (line == null))
 	return false;
 	else return true;
 	}
@@ -56,35 +68,34 @@ public class HouseFile {
 	// Gets and returns house information from the house info file
 	public static ListHouse getNextHouse() throws IOException
 	{
-	String lastName = "xxxxx";
-	String firstName = "xxxxx";
 	int lotNumber = 0;
+	String firstName = "";
+	String lastName = "";
 	int price = 0;
 	int squareFeet = 0;
 	int bedRooms =0;
-	
-	lastName = readString;
+	lastName = line;
 	firstName = readFile.readLine();
 	lotNumber = Integer.parseInt(readFile.readLine());
 	price = Integer.parseInt(readFile.readLine());
 	squareFeet = Integer.parseInt(readFile.readLine());
 	bedRooms = Integer.parseInt(readFile.readLine());
-	
-	readString = readFile.readLine();
-	ListHouse house = new ListHouse(lastName, firstName, lotNumber, price,	squareFeet, bedRooms);
+	line = readFile.readLine();
+	ListHouse house = new ListHouse(lotNumber, firstName, lastName, price,
+	squareFeet, bedRooms);
 	return house;
 	}
 	
 
 	// Puts parameter house information into the house info file
-	public static void putToFile(ListHouse house) throws IOException
+	public static void writeToFile(ListHouse house) throws IOException
 	{
-	writeFile.println(house.lastName());
-	writeFile.println(house.firstName());
-	writeFile.println(house.lotNumber());
-	writeFile.println(house.price());
-	writeFile.println(house.squareFeet());
-	writeFile.println(house.bedRooms());
+        writeFile.println(house.getLotNumber());    
+	writeFile.println(house.getFirstName());
+	writeFile.println(house.getLastName());
+	writeFile.println(house.getPrice());
+	writeFile.println(house.getSquareFeet());
+	writeFile.println(house.getBedRooms());
 	}
 	
 
--- RealEstate/src/org/realEstate/Listable.java
+++ RealEstate/src/org/realEstate/Listable.java
@@ -1,14 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package org.realEstate;
 
+/**
+ *
+ * @author user
+ */
 public interface Listable {
-	
-	// compares the current Listable object to other object,
-	// if those two objects are equal, this method returns a zero.
-	// if current object is < other object, returns a negative value.
-	// if current object is > other object, returns a positive value.
+    
+        //compares the current Listable object to other object, 
+	//if those two objects are equal, this method returns a zero.
+	//if current object is < other object, returns a negative value.
+	//if current object is > other object, returns a positive value.
 	public abstract int compareTo(Listable other);
 	
-	// returns a copy of the current Listable object
+	//returns a copy of the current Listable object
 	public abstract Listable copy();
-
 }
