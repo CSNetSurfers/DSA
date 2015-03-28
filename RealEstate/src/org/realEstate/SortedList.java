031a6d87772097b1cf750b3eadb792ab25f94dab
yashodaa <yashodara13@gmail.com>
2015-03-28 08:53:47 -0700

    SortedList

--- RealEstate/src/org/realEstate/SortedList.java
+++ RealEstate/src/org/realEstate/SortedList.java
@@ -8,7 +8,34 @@ package org.realEstate;
  *
  * @author user
  */
-public class SortedList {
+public class SortedList extends ListHouse{
     
+    //Constructor which takes and passes values of house information to super class constructor.
+	public SortedList(int lotNumber, String firstName, String lastName,
+			int price, int squareFeet, int bedRooms) {
+		super(lotNumber, firstName, lastName, price, squareFeet, bedRooms);
+	
+	}
+	
+	//default constructor
+	public SortedList(){
+		super();
+	}
+	
+	//returns whether the list is full
+	public boolean isFull(){
+		return (list.length == numHouses);
+	}
+
+	//returns the length of the list
+	public int size(){
+	return numHouses;
+	}
+	
+	//sets the current position of the list to its beginning
+	public void reset(){
+	currentIndex = 0;
+	}
+
     
 }
